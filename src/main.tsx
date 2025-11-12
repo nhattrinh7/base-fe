78f127ca5ddb7d493379926a6b70785558f83e72
Trinh Nhat <trinhminhnhatym@gmail.com>
2025-11-13 00:53:45 +0700

    add some more

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import CssBaseline from '@mui/material/CssBaseline'
-import App from './App.tsx'
+
 import theme from './theme.ts'
 import { ThemeProvider } from '@mui/material/styles'
+import router from './routes/index.ts'
+import { RouterProvider } from 'react-router-dom';
 
 // CssBaseline dùng để nhất quán css trên các trình duyệt
 // có thể dùng thêm 'material-ui-confirm' nếu cânz
@@ -13,7 +15,7 @@ import { ThemeProvider } from '@mui/material/styles'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme} disableTransitionOnChange>
-      <App />
+      <RouterProvider router={router} />
       <CssBaseline />
     </ThemeProvider>
   </StrictMode>,
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from 'react-router-dom'
+// import App from '../App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/abc',
+    // element:
+  }
+])
+
+export default router
